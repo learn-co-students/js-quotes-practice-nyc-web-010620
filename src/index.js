825ea6e4fded4b350bda535bec513529ff9fa981
github username <github email address>
2020-08-10 22:58:46 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -2,92 +2,110 @@ QOTES_URL = "http://localhost:3000/quotes"
 LIKES_URL = "http://localhost:3000/likes"
 BASE_URL ="http://localhost:3000/quotes?_embed=likes"
 
+document.addEventListener("DOMContentLoaded", e=>{
 
-fetch(BASE_URL)
-.then(resp =>resp.json())
-.then( quotes =>{quotes.forEach(addQuote) 
-})
-
-function addQuote(quote){
-
-    ul = document.getElementById('quote-list')
-    li = document.createElement('li')
-    li.className ='quote-card'
-    li.id = `${quote.id}`
-    blockquote = document.createElement('blockquote')
-    blockquote.className = 'blockquote'
-    blockquote.innerHTML = `
-    <p class="mb-0">${quote.quote}</p>
-    <footer class="blockquote-footer">${quote.author}</footer>
-    <br>
-    <button class='btn-success' data-purpose ="like">Likes: <span>${quote.likes.length}</span></button>
-    <button class='btn-danger' data-purpose ="delete">Delete</button>`
-
-    li.appendChild(blockquote)
-    ul.appendChild(li)
-
-}//add quote
-
-let form = document.getElementById('new-quote-form')
-
-form.addEventListener('submit', function(event){
-    event.preventDefault()
-    let quote = form.quote.value
-    let author = form.author.value
-    let likes = []
-
-    let body ={quote: quote, author:author, likes:likes}
-    addQuote(body)
-    fetch(BASE_URL, {
-        method: 'POST', 
-        headers:{
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'},
-        body: JSON.stringify(body)
+    let quoteUl = document.querySelector("#quote-list")
     
-        })//fetch
-})//submit new quote
-
-document.addEventListener("click", function(event){
-    if (event.target.dataset.purpose ==="delete")
-
-    {let block = event.target.parentNode
-        let li = block.parentNode
-        li.parentNode.removeChild(li)
+    fetch(BASE_URL)
+    .then(resp =>resp.json())
+    .then(data =>data.forEach(renderQuotes))
     
     
-        let quoteId = li.id
-
+    function renderQuotes(quote){
         
-        fetch(`${QOTES_URL}/${quoteId}`, {
-            method: 'DELETE', 
-        
-        })
-    } else if(event.target.dataset.purpose ==="like"){
-        // debugger
-        let span =  event.target.children[0]
-        let spanNumber = parseInt(span.innerText)
-        let newNumber = spanNumber+1
-        span.innerText = newNumber
-
-        let id = parseInt(event.target.parentNode.parentNode.id)
-
-        body ={quoteId: id}
-
-        fetch("http://localhost:3000/likes", {
-            method: 'POST', 
-            headers:{
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'},
-            body: JSON.stringify(body)
-        })
-        .then(resp => console.log(resp))
+        let quoteLi = document.createElement("li")
+        quoteLi.className = "quote-card"
+        quoteLi.innerHTML =`
+            <blockquote class="blockquote">
+            <p class="mb-0">${quote.quote}</p>
+            <footer class="blockquote-footer">${quote.author}</footer>
+            <br>
+            <button class='btn-success'>Likes: <span>${quote.likes.length}</span></button>
+            <button class='btn-danger' data-id=${quote.id}>Delete</button>
+            </blockquote>
+        `
+        quoteUl.appendChild(quoteLi)
     }
 
+   function addNewQuote(){
+
+        let form  = document.getElementById("new-quote-form")
+        form.addEventListener("submit", e=>{
+        
+            e.preventDefault()
+            let quote = e.target.quote.value
+            let author = e.target.author.value
+            let likes = []
+        
+        
+            let newQuote= {
+                quote: quote,
+                author: author,
+                likes: likes
+            }
+        
+            renderQuotes(newQuote)
+        
+            let options = {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify(newQuote)
+            }
+        
+            fetch(BASE_URL, options)
+        
+        })//eventListener
+    }//f addQuote
+    
+    
+    addNewQuote()
+    
+    //     // debugger
+    function toggleButtons(){
+
+        quoteUl.addEventListener("click", e=>{
+
+            if(e.target.innerText ==="Delete"){
+
+                let quoteId = e.target.dataset.id
+                let parentLi = e.target.parentNode.parentNode
+                let new_link = `http://localhost:3000/quotes/${quoteId}`
+                const option = {
+                    method: "DELETE"
+                }
+                fetch(new_link, option)  
+                .then(res =>{
+                    parentLi.remove()
+                })
+            } else {
+                console.log("likes")
+            }
+        })
+    } //f deleteQuote
+    
+    toggleButtons()
+    
+    function addLikes(){
+        ul.getElementById("click", e=>{
+            console.log("clicks")
+        })     
+    }
+    
+    addLikes()
 
 
 
-})//event listener 
+    
+
+    
+
+
+
+    
+})//DomLoaded
 
 
 
